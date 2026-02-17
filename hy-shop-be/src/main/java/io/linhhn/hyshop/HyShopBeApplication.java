6bf7e6ed0136f5023061a0c23bf3b821cefe8258
LinhHN <LinhHNDev@gmail.com>
2026-02-17 22:22:56 +0700

    init Backend

---
+++ hy-shop-be/src/main/java/io/linhhn/hyshop/HyShopBeApplication.java
@@ -0,0 +1,13 @@
+package io.linhhn.hyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyShopBeApplication.class, args);
+	}
+
+}
---
+++ hy-shop-be/src/test/java/io/linhhn/hyshop/HyShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.linhhn.hyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyShopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
